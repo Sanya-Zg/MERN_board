566813fb0accd304d9d493230de3c861904ec704
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-06-26 13:04:42 +0300

    create model Note

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import notesRoutes from './routes/notesRoutes.js';
-import { connectDB } from '../config/db.js';
+import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 dotenv.config();
 
