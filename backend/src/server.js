b20f86702dbf3d5d8768e3e6d97971a3ac21ddd7
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-06-28 20:21:12 +0300

    setting HomePage

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,20 @@ import { connectDB } from './config/db.js';
 import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
 dotenv.config();
+import cors from 'cors';
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 // middleware
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+  })
+);
 app.use(express.json());
 app.use(rateLimiter);
+
 
 // routes
 app.use('/api/notes', notesRoutes);
