d74c59598bb8483495741e5f6c988352978ef9e2
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-06-29 18:34:41 +0300

    prepered for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,22 +5,33 @@ import dotenv from 'dotenv';
 import rateLimiter from './middleware/rateLimiter.js';
 dotenv.config();
 import cors from 'cors';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 5001;
+const __dirname = path.resolve();
 
 // middleware
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-  })
-);
+if (process.env.NODE_ENV !== 'production') {
+  app.use(
+    cors({
+      origin: 'http://localhost:5173',
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
-
 // routes
 app.use('/api/notes', notesRoutes);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html'));
+  });
+}
 
 // connection to DB
 connectDB().then(() => {
