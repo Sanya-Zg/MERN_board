164f033caf4b5619ad7cf4972af0891834602cfa
Oleksandr Mas <sanya.maslovskii@gmail.com>
2025-06-29 14:00:52 +0300

    create delete functionality

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,8 +1,23 @@
 import { PenSquareIcon, Trash2Icon } from 'lucide-react';
 import { Link } from 'react-router';
 import { formatDate } from '../lib/utils';
+import api from '../lib/axios';
+import toast from 'react-hot-toast';
 
-const NoteCard = ({ note }) => {
+const NoteCard = ({ note, setNotes }) => {
+  const handleDelete = async (e, id) => {
+    e.preventDefault();
+
+    if (!window.confirm('Are you sure you want to delete this note?')) return;
+
+    try {
+      await api.delete(`/notes/${id}`);
+      setNotes((prev) => prev.filter((note) => note._id !== id));
+      toast.success('Note deleted successfully');
+    } catch (error) {
+      toast.error('Failed to delete note');
+    }
+  };
   return (
     <Link
       to={`note/${note._id}`}
@@ -12,11 +27,16 @@ const NoteCard = ({ note }) => {
         <h3 className="card-title text-base-content">{note.title}</h3>
         <p className="text-base-content/70 line-clamp-3">{note.content}</p>
         <div className="card-actions justify-between items-center mt-4">
-          <span className="text-sm text-base-content/60">{formatDate(new Date(note.createdAt))}</span>
+          <span className="text-sm text-base-content/60">
+            {formatDate(new Date(note.createdAt))}
+          </span>
           <div className="flex items-center gap-1">
-            <PenSquareIcon className='size-4'/>
-            <button className='btn btn-ghost btn-xs text-error'>
-              <Trash2Icon className='size-4'/>
+            <PenSquareIcon className="size-4" />
+            <button
+              className="btn btn-ghost btn-xs text-error"
+              onClick={(e) => handleDelete(e, note._id)}
+            >
+              <Trash2Icon className="size-4" />
             </button>
           </div>
         </div>
